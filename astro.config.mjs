2ec667c769ae13904c83ce029f34fc567d6f2981
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2025-08-14 10:59:31 +0300

    Added all files for all topics and others

---
+++ astro.config.mjs
@@ -0,0 +1,342 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeRapide from "starlight-theme-rapide";
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+      title: "SAMA",
+      favicon: "/public/favicon.ico",
+      customCss: ["./src/styles/custom.css"],
+      logo: {
+        light: "./src/assets/header-sama-light.svg",
+        dark: "./src/assets/header-sama-dark.svg",
+        replacesTitle: true,
+      },
+      components: {
+        SocialIcons: "./src/components/CustomSocialIcons.astro",
+        Header: "./src/components/CustomHeader.astro",
+      },
+      social: [
+        {
+          icon: "comment",
+          label: "Medium",
+          href: "https://medium.com/sama-communications",
+        },
+        {
+          icon: "npm",
+          label: "NPM",
+          href: "https://www.npmjs.com/package/@sama-communications/sdk",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/ye68KMgA2f",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/SAMA-Communications/sama-server",
+        },
+        {
+          icon: "rocket",
+          label: "Help us!",
+          href: "https://buymeacoffee.com/khomenkoigor",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Home",
+          link: "/",
+          attrs: {
+            style:
+              "font-size: var(--sl-text-base); font-weight: 600; color: var(--sl-color-white);",
+          },
+        },
+        {
+          label: "Overview",
+          items: [
+            { label: "Introduction", link: "/overview/introduction/" },
+            { label: "Quick Start", link: "/overview/quick-start/" },
+            { label: "Roadmap", link: "/overview/roadmap/" },
+            { label: "Contributing", link: "/overview/contributing/" },
+            { label: "FAQ", link: "/overview/faq/" },
+          ],
+        },
+        {
+          label: "Deployment",
+          items: [
+            {
+              label: "Manual Server Setup",
+              link: "/deployment/manual-server-setup/",
+            },
+            {
+              label: "Docker Server Setup",
+              link: "/deployment/docker-server-setup/",
+            },
+            { label: "Client-Side SDK", link: "/deployment/client-sdk/" },
+            { label: "DataBase Records", link: "/deployment/db-records/" },
+          ],
+        },
+        {
+          label: "API",
+          items: [
+            {
+              label: "User",
+              collapsed: true,
+              items: [
+                { label: "Create", link: "/api/wss/user/create/" },
+                { label: "Login", link: "/api/wss/user/login/" },
+                { label: "Logout", link: "/api/wss/user/logout/" },
+                { label: "Edit", link: "/api/wss/user/edit/" },
+                { label: "Delete", link: "/api/wss/user/delete/" },
+                { label: "Search", link: "/api/wss/user/search/" },
+                { label: "Get by IDs", link: "/api/wss/user/get-by-ids/" },
+              ],
+            },
+            {
+              label: "Address Book",
+              collapsed: true,
+              items: [
+                { label: "Add", link: "/api/wss/address-book/add/" },
+                {
+                  label: "Batch Add",
+                  link: "/api/wss/address-book/batch-add/",
+                },
+                { label: "Update", link: "/api/wss/address-book/update/" },
+                { label: "List", link: "/api/wss/address-book/list/" },
+                { label: "Delete", link: "/api/wss/address-book/delete/" },
+              ],
+            },
+            {
+              label: "Conversation",
+              collapsed: true,
+              items: [
+                { label: "Create", link: "/api/wss/conversation/create/" },
+                { label: "Edit", link: "/api/wss/conversation/edit/" },
+                {
+                  label: "Subscribe",
+                  link: "/api/wss/conversation/subscribe/",
+                },
+                {
+                  label: "Unsubscribe",
+                  link: "/api/wss/conversation/unsubscribe/",
+                },
+                { label: "Delete", link: "/api/wss/conversation/delete/" },
+                { label: "List", link: "/api/wss/conversation/list/" },
+                {
+                  label: "Participants List",
+                  link: "/api/wss/conversation/participants-list/",
+                },
+                { label: "Search", link: "/api/wss/conversation/search/" },
+              ],
+            },
+            {
+              label: "Conversation Handler",
+              collapsed: true,
+              items: [
+                {
+                  label: "Create",
+                  link: "/api/wss/conversation-handler/create/",
+                },
+                { label: "Get", link: "/api/wss/conversation-handler/get/" },
+                {
+                  label: "Delete",
+                  link: "/api/wss/conversation-handler/delete/",
+                },
+              ],
+            },
+            {
+              label: "Message",
+              collapsed: true,
+              items: [
+                {
+                  label: "Send & Receive",
+                  link: "/api/wss/message/send-receive/",
+                },
+                {
+                  label: "System Message",
+                  link: "/api/wss/message/system-message/",
+                },
+                { label: "List", link: "/api/wss/message/list/" },
+                { label: "Read", link: "/api/wss/message/read/" },
+                { label: "Typing", link: "/api/wss/message/typing/" },
+                { label: "Delete", link: "/api/wss/message/delete/" },
+                { label: "Edit", link: "/api/wss/message/edit/" },
+                {
+                  label: "Update Reaction",
+                  link: "/api/wss/message/update-reaction/",
+                },
+                {
+                  label: "List Reaction",
+                  link: "/api/wss/message/list-reaction/",
+                },
+              ],
+            },
+            {
+              label: "Push Subscription",
+              collapsed: true,
+              items: [
+                {
+                  label: "Create",
+                  link: "/api/wss/push-subscription/create/",
+                },
+                { label: "List", link: "/api/wss/push-subscription/list/" },
+                {
+                  label: "Delete",
+                  link: "/api/wss/push-subscription/delete/",
+                },
+                {
+                  label: "Push Event",
+                  link: "/api/wss/push-subscription/push-event/",
+                },
+              ],
+            },
+            {
+              label: "Carbons",
+              collapsed: true,
+              items: [
+                {
+                  label: "Info",
+                  link: "/api/wss/carbons/info",
+                },
+              ],
+            },
+            {
+              label: "Last Activity",
+              collapsed: true,
+              items: [
+                {
+                  label: "Get by IDs",
+                  link: "/api/wss/last-activity/get-by-ids/",
+                },
+                {
+                  label: "Subscribe",
+                  link: "/api/wss/last-activity/subscribe/",
+                },
+              ],
+            },
+            {
+              label: "Operation Logs",
+              collapsed: true,
+              items: [
+                {
+                  label: "Info",
+                  link: "/api/wss/operation-logs/info",
+                },
+              ],
+            },
+            {
+              label: "Files",
+              collapsed: true,
+              items: [
+                {
+                  label: "Create Upload URL",
+                  link: "/api/wss/files/create-upload-url/",
+                },
+                {
+                  label: "Get Download URL",
+                  link: "/api/wss/files/get-download-url/",
+                },
+              ],
+            },
+            {
+              label: "Block Users",
+              collapsed: true,
+              items: [
+                { label: "Block", link: "/api/wss/block-users/block/" },
+                { label: "Unblock", link: "/api/wss/block-users/unblock/" },
+                { label: "List", link: "/api/wss/block-users/list/" },
+              ],
+            },
+          ],
+        },
+        {
+          label: "HTTP API",
+          collapsed: true,
+          items: [
+            {
+              label: "POST",
+              collapsed: true,
+              items: [
+                {
+                  label: "/admin/message",
+                  link: "/api/http/post/admin-message/",
+                },
+                {
+                  label: "/admin/message/system",
+                  link: "/api/http/post/admin-message-system/",
+                },
+                {
+                  label: "/admin/activity/online",
+                  link: "/api/http/post/admin-activity-online/",
+                },
+              ],
+            },
+            {
+              label: "PUT",
+              collapsed: true,
+              items: [
+                {
+                  label: "/admin/message/read",
+                  link: "/api/http/put/admin-message-read/",
+                },
+                {
+                  label: "/admin/message",
+                  link: "/api/http/put/admin-message/",
+                },
+                {
+                  label: "/admin/message/reaction",
+                  link: "/api/http/put/admin-message-reaction/",
+                },
+              ],
+            },
+            {
+              label: "DELETE",
+              collapsed: true,
+              items: [
+                {
+                  label: "/admin/message",
+                  link: "/api/http/delete/admin-message/",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "Features",
+          items: [
+            // { label: "Encryption", link: "/features/encryption/" },
+            { label: "Customization", link: "/features/customization/" },
+            {
+              label: "Programmable Chat",
+              link: "/features/programmable-chat",
+            },
+            { label: "Dashboard", link: "/features/dashboard/" },
+            { label: "Clustering", link: "/features/clustering/" },
+            { label: "Load Testing", link: "/features/load-tests/" },
+          ],
+        },
+        {
+          label: "GitHub",
+          link: "https://github.com/SAMA-Communications/sama-server",
+          attrs: {
+            target: "_blank",
+            style:
+              "font-size: var(--sl-text-base); font-weight: 600; color: var(--sl-color-white);",
+          },
+        },
+        {
+          label: "Discrod",
+          link: "https://discord.gg/ye68KMgA2f",
+          attrs: {
+            target: "_blank",
+            style:
+              "font-size: var(--sl-text-base); font-weight: 600; color: var(--sl-color-white);",
+          },
+        },
+      ],
+    }),
+  ],
+});
